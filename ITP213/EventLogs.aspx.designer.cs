f51a11fc91e4be9dd913e5c11350377f2a1e2e6d
Lee Kai Ming <170195F@mymail.nyp.edu.sg>
2019-06-17 08:37:58 +0800

    Search Function
    Added a partially completed search function for event log

--- ITP213/EventLogs.aspx.designer.cs
+++ ITP213/EventLogs.aspx.designer.cs
@@ -13,13 +13,22 @@ namespace ITP213 {
     public partial class EventLogs {
         
         /// <summary>
-        /// GridView1 control.
+        /// DDEventLogList control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
+        protected global::System.Web.UI.WebControls.DropDownList DDEventLogList;
+        
+        /// <summary>
+        /// GVEventLog control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GVEventLog;
         
         /// <summary>
         /// SqlDataSourceEventLog control.
