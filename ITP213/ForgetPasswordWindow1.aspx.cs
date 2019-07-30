59675b42ecce5a40e0ee80ae91cf070654d4f9ba
wycliff99 <wycliff_lee@hotmail.com>
2019-07-30 16:07:50 +0800

    salt hash pw stuff and broken sending email

--- ITP213/ForgetPasswordWindow1.aspx.cs
+++ ITP213/ForgetPasswordWindow1.aspx.cs
@@ -10,6 +10,11 @@ using System.Configuration;
 using System.Drawing;
 using System.Net;
 using System.Net.Mail;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 //test
 namespace ITP213
 {
@@ -19,12 +24,56 @@ namespace ITP213
         {
 
         }
+        public Tuple<string, string> hashingAndSaltingPassword(string pwd)
+        {
+            //hashing & salting pwd
+            string finalHash;
+            string salt;
+
+            string password = pwd.Trim();
+
+            // Generate random "salt"
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] saltByte = new byte[8];
+
+            // Fills array of bytes with a cryptographically strong sequence of random values;
+            rng.GetBytes(saltByte);
+            salt = Convert.ToBase64String(saltByte);
+
+            SHA512Managed hashing = new SHA512Managed();
+
+            string pwdWithSalt = password + salt;
+            byte[] plainHash = hashing.ComputeHash(Encoding.UTF8.GetBytes(password));
+            byte[] hashWithSalt = hashing.ComputeHash(Encoding.UTF8.GetBytes(pwdWithSalt));
+
+            finalHash = Convert.ToBase64String(hashWithSalt);
+
+            /* RijndaelManaged cipher = new RijndaelManaged();
+            cipher.GenerateKey();
+            Key = cipher.Key;
+            IV = cipher.IV;*/
+
+            return Tuple.Create(finalHash, salt);
+        }
+
+        /*public static string CreateRandomPassword(int PasswordLength)
+        {
+            string _allowedChars = "0123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ";
+            Random randNum = new Random();
+            char[] chars = new char[PasswordLength];
+            int allowedCharCount = _allowedChars.Length;
+            for (int i = 0; i < PasswordLength; i++)
+            {
+                chars[i] = _allowedChars[(int)((_allowedChars.Length) * randNum.NextDouble())];
+            }
+            return new string(chars);
+        }*/
 
         protected void btnResetpw_Click(object sender, EventArgs e)
         {
             string mainconn = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
             SqlConnection sqlconn = new SqlConnection(mainconn);
-            string sqlquery = "select email,password from account where email=@email";
+            string sqlquery = "select email from account where email=@email";
             SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn);
             sqlcomm.Parameters.AddWithValue("@email", tbEmail.Text);
             sqlconn.Open();
@@ -32,31 +81,41 @@ namespace ITP213
             if (sdr.Read())
             {
                 string username = sdr["email"].ToString();
-                string password = sdr["password"].ToString();
 
-                MailMessage mm = new MailMessage("Someonesemail@gmail.com", tbEmail.Text);
-                mm.Subject = "Your new password";
-                mm.Body=string.Format("Hello : <h1>{0}</h1>, your password is <h1>{1}</h1>", username, password);
-                mm.IsBodyHtml = true;
-                SmtpClient smtp = new SmtpClient();
-                smtp.Host = "smtp.gmail.com";
-                smtp.EnableSsl = true;
-                NetworkCredential nc = new NetworkCredential();
-                nc.UserName = "Someonesemail@gmail.com";
-                nc.Password = "somepassword";
-                smtp.UseDefaultCredentials = true;
-                smtp.Credentials = nc;
-                smtp.Port = 587;
-                smtp.Send(mm);
-                LabelMsg.Text = "Your password has been sent to" + tbEmail.Text;
-                LabelMsg.ForeColor = Color.Green;
+                //string password = sdr["password"].ToString();
+
+
+
+                try
+                {
+                    MailMessage mm = new MailMessage("wycliff1999@gmail.com", tbEmail.Text);
+                    mm.Subject = "Your new password";
+                    mm.Body = string.Format("Hello : <h1>{0}</h1>, your password is <h1></h1>", username);
+                    mm.IsBodyHtml = true;
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.EnableSsl = true;
+                    /*NetworkCredential nc = new NetworkCredential();*/
+                    NetworkCredential loginInfo = new NetworkCredential(Convert.ToString(ConfigurationManager.AppSettings["wycliff1999@gmail.com"]), Convert.ToString(ConfigurationManager.AppSettings["wywy12345"]));
+                   /* nc.UserName = "170998G@mymail.nyp.edu.sg";
+                    nc.Password = "Wywy12345";*/
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = loginInfo;
+                    smtp.Port = 587;
+                    smtp.Send(mm);
+                    LabelMsg.Text = "Your password has been sent to" + tbEmail.Text;
+                    LabelMsg.ForeColor = Color.Green;
+                }
+                catch (Exception ex) { throw new Exception(ex.ToString()); }
             }
             else
             {
                 LabelMsg.Text = "Email does not exist";
                 LabelMsg.ForeColor = Color.Red;
             }
-
+            
+            
+            
         }
     }
 }
